6ba6fa814d4bba0f5d8e6dc875e28d5d7653b7a2
RITUL MALVIYA <ritulmalviya2612@gmail.com>
2021-08-05 15:27:46 +0530

    My second commit

--- Main.java
+++ Main.java
@@ -3,5 +3,18 @@ class Main
    public static void main(String args[])
 {
      System.out.println("Welcome");
+     System.out.println("Is number prime ? "+isPrime(7));
 }
+public static int isPrime(int num)
+ {
+    for(int i=2;i<num/2;i++)
+  {
+        if(num%i==0)
+         {
+            return false;
+            break;
+              }
+   }
+    return true;
+ }
 }
